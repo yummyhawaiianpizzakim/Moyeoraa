7ffe749ab9fa5ca6201b728e4fa598e57d0a7326
yummyhawaiianpizzakim <yummyhawaiianpizzakim@gmail.com>
2024-05-12 17:55:11 +0900

    bindEmptyView 메서드 구현 // chatListFeature = emptyView 적용

--- Projects/App/Sources/Feature/HomeFeature/Sources/ViewController/HomeFeature.swift
+++ Projects/App/Sources/Feature/HomeFeature/Sources/ViewController/HomeFeature.swift
@@ -168,11 +168,7 @@ public final class HomeFeature: BaseFeature {
         let output = self.viewModel.trnasform(input: input)
         
         output.Plans.drive(with: self) { owner, plansArr in
-            if !plansArr.isEmpty {
-                owner.emptyView.isHidden = true
-            } else {
-                owner.emptyView.isHidden = false
-            }
+            owner.bindEmptyView(isEmpty: plansArr.isEmpty)
             
             let snap = owner.setSnapshot(plans: plansArr)
             owner.dataSource?.apply(snap)
@@ -184,6 +180,14 @@ public final class HomeFeature: BaseFeature {
 }
 
 private extension HomeFeature {
+    func bindEmptyView(isEmpty: Bool) {
+        if !isEmpty {
+            self.emptyView.isHidden = true
+        } else {
+            self.emptyView.isHidden = false
+        }
+    }
+    
     func updateScrollViewContentSize() {
         let collectionViewHeight = self.plansCollectionView.contentSize.height
         let contentViewHeight = self.contentView.frame.height
